7bde6ef63641c54479f5cfc612b9affb227705ff
kimyougmin <dudals896@gmail.com>
2023-11-08 20:57:09 +0900

    feat: Implement login and signup feature

--- src/loginScreen/LoginScreen.tsx
+++ src/loginScreen/LoginScreen.tsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
+import useCookie from 'react-use-cookie'
 
 interface userForm {
-  userId: string
+  userName: string
   userPw: string
 }
 function LoginScreen (): React.JSX.Element {
   const [userFrom, setUserForm] = useState<userForm>({
-    userId: '',
+    userName: '',
     userPw: ''
   })
+  const [, setCookies] = useCookie('userData')
 
   const onChangeInputValue = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setUserForm({ ...userFrom, [event.target.name]: event.target.value })
+  }
+  const loginEventHandel = (): void => {
+    fetch('http://localhost:8080/login', {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ name: userFrom.userName, pw: userFrom.userPw })
+    }).then((res) => {
+      setCookies(JSON.stringify(res))
+    }).catch(() => {
+      alert('로그인 실패!')
+    })
   }
   return (
         <div>
             <div>
                 <h1>Login</h1>
                 <p>enter your details to sign in to your account</p>
-                <input value={userFrom.userId} name={'userId'} onChange={(event) => { onChangeInputValue(event) }}/>
+                <input value={userFrom.userName} name={'userId'} onChange={(event) => { onChangeInputValue(event) }}/>
                 <input value={userFrom.userPw} name={'userPw'} onChange={(event) => { onChangeInputValue(event) }}/>
-                <button>Login In</button>
+                <button onClick={loginEventHandel}>Login In</button>
                 <p>Don＇t have an account? Signup Now?</p>
             </div>
         </div>
--- src/signUpScreen/SignUpScreen.tsx
+++ src/signUpScreen/SignUpScreen.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { useNavigate } from 'react-router-dom'
+import { Button, Input } from '@mui/material'
 
+interface userFrom {
+  id: string
+  name: string
+  pw: string
+}
 function SignUpScreen (): React.JSX.Element {
+  const [userForm, setUserForm] = useState<userFrom>({
+    id: uuidv4(),
+    name: '',
+    pw: ''
+  })
+  const navi = useNavigate()
+  const onChangeInputValue = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
+    setUserForm({ ...userForm, [event.target.name]: event.target.value })
+  }
+  const signUpEventHandel = (): void => {
+    fetch('http://localhost:8080/login', {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ id: userForm.id, name: userForm.name, pw: userForm.pw })
+    }).then((res) => {
+      if (res.status === 200) {
+        navi('/')
+      }
+    }).catch(() => {
+      alert('로그인 실패!')
+    })
+  }
   return (
-        <div>
-
+        <div className={'singUpScreen'}>
+            <Input name={'name'}
+                   value={userForm.name}
+                   placeholder={'닉네임을 입력해주세요'}
+                   onChange={(event) => { onChangeInputValue(event) }}/>
+          <Input name={'name'}
+                 value={userForm.pw}
+                 placeholder={'비밀번호을 입력해주세요'}
+                 onChange={(event) => { onChangeInputValue(event) }}/>
+          <Button onClick={signUpEventHandel}>회원 가입</Button>
         </div>
   )
 }
