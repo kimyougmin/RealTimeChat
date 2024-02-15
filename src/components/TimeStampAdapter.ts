9cd5e7d618814de09d803351f3466344a2bcf43a
kimyougmin <dudals896@gmail.com>
2024-02-15 13:27:25 +0900

    소켓 통신의 시간을 타입스템프와 동일하게 변경
    ex)
    20??-??-??T??:??:??

--- src/components/TimeStampAdapter.ts
+++ src/components/TimeStampAdapter.ts
@@ -8,5 +8,5 @@ export default function TimeStampAdapter (timestamp): string {
   const minutes = String(date.getMinutes()).padStart(2, '0')
   const seconds = String(date.getSeconds()).padStart(2, '0')
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`
 }
